e02583228e334026d797951c273ba420ff89b651
shaggysa <81648551+shaggysa@users.noreply.github.com>
2025-08-12 09:23:58 -0500

    Update testing file to include puzzle generation

--- testing.py
+++ testing.py
@@ -1,11 +1,20 @@
 import lib_sudoku as sudoku
+import time
 
 
 def tests():
-    reader = sudoku.PuzzleReader("puzzles.csv")
-
+    reader = sudoku.PuzzleReader(
+        "https://raw.githubusercontent.com/shaggysa/lib_sudoku/master/puzzles.csv", True
+    )
     sudoku.async_speedtest(reader)
     sudoku.synchronous_speedtest(reader)
+
+    num_hints = 24
+    start_gen = time.time()
+    sudoku.gen_unsolved(num_hints)
+    print(
+        f"Generated a puzzle with {num_hints} hints in {(time.time() - start_gen)*1000} milliseconds."
+    )
 
 
 if __name__ == "__main__":
